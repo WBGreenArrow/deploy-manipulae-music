18a8cf1c8870ebb0ca9e684c9aa4ed50f33f1c70
wellison E Brito <wellysone.brito@gmail.com>
2021-10-10 15:22:19 -0300

    changed api router

--- manipulae_music_webapp/src/services/api.js
+++ manipulae_music_webapp/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://deploy-manipulae-music.vercel.app",
 });
 
 export default api;
